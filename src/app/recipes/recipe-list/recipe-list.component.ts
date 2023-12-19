f6eddf973bc09bccc9b971a59d6db28c47603eb1
JaviOGDev <javierortgar145@gmail.com>
2023-12-19 12:06:41 +0100

    Created header, shopping and ingredient components. Added some styles

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG'
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg'
+    ),
+  ];
+}
