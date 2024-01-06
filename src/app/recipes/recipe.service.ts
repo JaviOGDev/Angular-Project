dffcd846f98413e5c4ea24953b89314e98dc2f3c
JaviOGDev <javierortgar145@gmail.com>
2024-01-06 17:35:15 +0100

    Created database on firebase. Added Http requests to fetch data and save data.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,20 +8,22 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Spaghetti Bolognese',
-      'A super Spaghetti Bolognese',
-      'https://cdn2.cocinadelirante.com/sites/default/files/images/2023/06/espaguettialabolognesa.jpg',
-      [new Ingredient('Meat', 5), new Ingredient('Pasta', 100)]
-    ),
-    new Recipe(
-      'Big Burger',
-      'A delicious big and juicy burger',
-      'https://www.foodandwine.com/thmb/DI29Houjc_ccAtFKly0BbVsusHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/crispy-comte-cheesburgers-FT-RECIPE0921-6166c6552b7148e8a8561f7765ddf20b.jpg',
-      [new Ingredient('Bread', 2), new Ingredient('Meat', 1)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Spaghetti Bolognese',
+  //     'A super Spaghetti Bolognese',
+  //     'https://cdn2.cocinadelirante.com/sites/default/files/images/2023/06/espaguettialabolognesa.jpg',
+  //     [new Ingredient('Meat', 5), new Ingredient('Pasta', 100)]
+  //   ),
+  //   new Recipe(
+  //     'Big Burger',
+  //     'A delicious big and juicy burger',
+  //     'https://www.foodandwine.com/thmb/DI29Houjc_ccAtFKly0BbVsusHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/crispy-comte-cheesburgers-FT-RECIPE0921-6166c6552b7148e8a8561f7765ddf20b.jpg',
+  //     [new Ingredient('Bread', 2), new Ingredient('Meat', 1)]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
@@ -50,6 +52,11 @@ export class RecipeService {
 
   deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
     this.recipesChanged.next(this.recipes.slice());
   }
 }
